27020202f82eb72fcd6a5586c756c20fd0143fe1
Maya Clancy <mclancy@albany.edu>
2025-03-24 20:07:20 -0400

    Add files via upload

---
+++ output.js
@@ -0,0 +1,15 @@
+function show InnerHTML() {
+document.getElementByID("output1").innerHTML = "Hello from innerHTML!";
+}
+
+function useDocumentWrite() {
+document.write("This replaces the page content! Not recommended.");
+}
+
+function showAlert() {
+alert("This is an alert box!");
+}
+
+function showConsole() {
+console.log("This message is in the console.");
+}
---
+++ variables.js
@@ -0,0 +1,15 @@
+// Using Let
+let name = "Crystal";
+let age = "unknown";
+let isStudent  true;
+
+document.getElementId("demo1").innerHTML =
+"Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+
+//var city = "Albany";  // Function-scoped
+//let state = "NY";  // Block-scoped
+//const country = "USA";  // Cannot be reassigned
+
+//document.getElementById("demo2").innerHTML =
+//  "Location: " + city + ", " + state + ", " + country;
